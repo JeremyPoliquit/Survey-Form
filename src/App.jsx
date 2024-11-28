1222c374471dd3e3ec189353aa9492ba1d548a16
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-11-28 00:28:21 -0800

    Card Input with Next Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import CardInput from "./components/cardInput";
+import NextButton from './components/nextButton'
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Form Submitted:`);
+  };
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col gap-6 justify-center items-center min-h-screen bg-slate-700 text-white">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-1/2 mx-auto">
+        <CardInput
+          content="Register Username:"
+          description="Your Username"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <CardInput
+          content="Register Password:"
+          description="Password"
+          type="text"
+          name="Your password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <NextButton />
+
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
