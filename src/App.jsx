6024410afb4b013fa21dbbb9b5a5726262d5db06
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-11-28 19:10:17 -0800

    Link content pages new implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,21 @@
 import React, { useState } from "react";
-import CardInput from "./components/cardInput";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Introduction";
+import Questions from './pages/Questions'
+import CreateForm from './pages/Client/creatingForm'
 
 function App() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Form Submitted:`);
-  };
 
   return (
-    <div className="flex flex-col gap-6 justify-center items-center min-h-screen bg-slate-700 text-white">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-1/2 mx-auto">
-        <CardInput
-          content="Register Username:"
-          description="Your Username"
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-        <CardInput
-          content="Register Password:"
-          description="Password"
-          type="text"
-          name="Your password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-
-
-      </form>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/createform" element={<CreateForm />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
